f5df84efa826074e9a64cd561bd8fec8c5b811d6
DudaaMoura <mariamourapalmital@gmail.com>
2023-05-09 15:09:25 +0000

    ingresso

---
+++ IngressoVip.java
@@ -0,0 +1,29 @@
+public class IngressoVip extends Ingresso {
+
+    private float valorAdicional;
+
+    public void setValorAdicional(float valorAdicional) {
+        this.valorAdicional = valorAdicional;
+    }
+
+    public float getValorAdicional() {
+        return valorAdicional;
+    }
+    
+    
+    public IngressoVip(float valor, float valorAdicional){
+        super();
+        this.valorAdicional = valorAdicional;
+
+    }
+    public IngressoVip(){
+
+    }
+
+    public String toString(){
+        return "O valor total é:"+(valorAdicional+getValor());
+    }
+
+
+    
+}
